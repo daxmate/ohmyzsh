7e7dc10ada8129a68a66cd738d4cec53172f4c73
dax <dax26d@gmail.com>
2024-05-10 13:08:59 +0800

    added aliases for home brew

--- plugins/dax/dax.plugin.zsh
+++ plugins/dax/dax.plugin.zsh
@@ -1,1 +1,13 @@
 alias rmfr="rm -fr"
+
+# home brew alias
+alias br=brew
+alias brins="brew install"
+alias brdoc="brew doctor"
+alias bruin="brew uninstall"
+alias brinf="brew info"
+alias brsch="brew search"
+alias brout="brew outdated"
+alias brupg="brew upgrade"
+alias brupd="brew update"
+alias brlst="brew list"
